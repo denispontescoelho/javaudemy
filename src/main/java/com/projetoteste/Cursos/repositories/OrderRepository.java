83144ecf495ad1eda29b5afa9607025ba7f37919
Denis Pontes <denispontescoelho@gmail.com>
2022-11-20 14:52:54 -0300

    Order, Instant, ISO 8601-parte2

---
+++ src/main/java/com/projetoteste/Cursos/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.projetoteste.Cursos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoteste.Cursos.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+	
+	
+
+}
