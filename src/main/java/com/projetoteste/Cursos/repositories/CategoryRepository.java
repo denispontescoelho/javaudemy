0d95d37cf87993c78064a9fe167f29ff231cbb7b
Denis Pontes <denispontescoelho@gmail.com>
2022-11-21 18:07:54 -0300

    Category

---
+++ src/main/java/com/projetoteste/Cursos/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.projetoteste.Cursos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoteste.Cursos.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+	
+	
+
+}
